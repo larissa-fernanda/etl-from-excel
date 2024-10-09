e76df8b6b415c6211e41fdee932a7ee0a20bc0de
larissa-fernanda <laliffr@gmail.com>
2024-10-09 19:34:10 -0300

    feat: Adiciona função main

---
+++ etl_from_excel/etl.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from treat_data import treat_data_pipeline
+from send_to_airtable import send_to_airtable_pipeline
+
+load_dotenv()
+
+PATH = os.getenv('FILEPATH')
+FILENAME = os.getenv('FILENAME')
+SHEET_NAME = os.getenv('SHEET_NAME')
+HEADER_ROW = int(os.getenv('HEADER_ROW'))
+QUANTITY_COLUMNS = os.getenv('QUANTITY_COLUMNS').split(',') if os.getenv('QUANTITY_COLUMNS') else None
+DATE_COLUMNS = os.getenv('DATE_COLUMNS').split(',') if os.getenv('DATE_COLUMNS') else None
+COLUMNS_TO_SELECT = os.getenv('COLUMNS_TO_SELECT').split(',') if os.getenv('COLUMNS_TO_SELECT') else None
+
+def main():
+    print('Starting ETL process...')
+
+    df = treat_data_pipeline(
+        path=PATH,
+        filename=FILENAME,
+        sheet_name=SHEET_NAME,
+        header=HEADER_ROW,
+        quantity_column=QUANTITY_COLUMNS,
+        date_column=DATE_COLUMNS,
+        columns_to_select=COLUMNS_TO_SELECT
+    )
+
+    send_to_airtable_pipeline(df)
+
+if __name__ == '__main__':
+    main()
